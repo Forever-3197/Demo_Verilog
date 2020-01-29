3150f2fc9eb1c91046822628f505f675ac9160c5
Forever-3197 <1311735077@qq.com>
2020-01-29 19:00:18 +0800

    第一个工程　流水灯

--- demo.v
+++ demo.v
@@ -1,6 +1,26 @@
 module led(
-    input wire sclk
+    input wire clk,
+    input wire rst_n,
 
+    output wire[3:0] led_o
 );
+
+reg [30:0]  cnt;
+reg [3:0] led_reg;
+always@(posedge clk)
+if (!rst_n) 
+    cnt <= 0;
+else if(cnt == 31'd24_999_999)
+    cnt <= 0;
+else
+    cnt <= cnt + 1;
+
+always@(posedge clk)
+if(!rst_n)
+    led_reg <= 4'b0001;
+else if(cnt == 0)
+    led_reg <= {led_reg[2:0],led_reg[3]};
+
+assign led_o = led_reg;
 
 endmodule
